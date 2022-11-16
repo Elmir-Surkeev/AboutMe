f0550434be65fbc4500439a0b9900019754724d0
Elmir Surkee <elmirsurkeev@gmail.com>
2022-11-16 23:16:14 +0600

    deleted bad comment

--- src/MyPhoto.jsx
+++ src/MyPhoto.jsx
@@ -5,20 +5,7 @@ import Photo3 from "./UI/third.jpg";
 import Photo4 from "./UI/fourth.jpg";
 import Photo5 from "./UI/fifth.jpg";
 import Photo6 from "./UI/sixth.jpg";
-// var mid = document.getElementById("smena-snimka");
-// var mid_off = mid.offsetTop - 150;
-// window.addEventListener("scroll", function () {
-//   if (
-//     document.body.scrollTop > mid_off ||
-//     document.documentElement.scrollTop > mid_off
-//   ) {
-//     mid.classList.remove("smena-snimka-1");
-//     mid.classList.add("smena-snimka-2");
-//   } else {
-//     mid.classList.remove("smena-snimka-2");
-//     mid.classList.add("smena-snimka-1");
-//   }
-// });
+
 const MyPhoto = () => {
   return (
     <div className="main-picture-change">
@@ -26,13 +13,4 @@ const MyPhoto = () => {
     </div>
   );
 };
-
 export default MyPhoto;
-
-{
-  /* //   return (
-  //     <div className="main-picture-change">
-        // <img className="myPhoto" src={Photo} alt="My photo" />
-  //     </div>
-  //   ) */
-}
