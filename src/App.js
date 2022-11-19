0ec2c42af0e31a80bedbd615cf4576aed198956c
Elmir Surkee <elmirsurkeev@gmail.com>
2022-11-19 21:38:16 +0600

    changed app.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Header from "./Header";
 import Main from "./Main";
-
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Main></Main>
+      <Header />
+      <Main />
     </div>
   );
 }
